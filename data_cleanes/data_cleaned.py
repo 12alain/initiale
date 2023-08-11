395c119819324a4c815ec1d3ad6bfecafa5991d5
12alain <126027487+12alain@users.noreply.github.com>
2023-08-11 13:47:33 +0000

    Update data_cleaned.py

--- data_cleanes/data_cleaned.py
+++ data_cleanes/data_cleaned.py
@@ -5,3 +5,6 @@ import seaborn as sb
 import matplotlib.pyplot as plt
 #  chargement des donnees 
 df=pd.read_csv('./data/billets.csv',sep=";")
+# correction des valeurs manquantes
+df['margin_low'].fillna(value=df['margin_low'].mean(),inplace=True)
+df.to_csv('./data_cleanes/data_cleaned.csv', index=False)
