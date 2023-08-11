6a230bdc34745813b27af87cf1907794d14b6270
12alain <126027487+12alain@users.noreply.github.com>
2023-08-11 14:14:24 +0000

    Update model.py

--- model.py
+++ model.py
@@ -4,8 +4,6 @@ import numpy as np
 import seaborn as sb
 import matplotlib.pyplot as plt
 df = pd.read_csv('./data_cleanes/data_cleaned.csv', sep=";")
-# correction des valeurs manquantes
-df['margin_low'].fillna(value=df['margin_low'].mean(),inplace=True)
 df['is_genuine'].astype('int')
 from sklearn.model_selection import train_test_split
 x=df[["diagonal","height_left","height_right","margin_low","margin_up","length"]]
