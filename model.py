dc38982d808837e68da60eb42f55aea7eaa4ee05
12alain <126027487+12alain@users.noreply.github.com>
2023-08-11 15:27:44 +0000

    Update model.py

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sb
 import matplotlib.pyplot as plt
-df = pd.read_csv('./data_cleanes/data_cleaned.csv', sep=";")
+df = pd.read_csv('./data_cleanes/data_cleaned.csv', sep=",")
 #df['is_genuine'].astype('int')
 from sklearn.model_selection import train_test_split
 x=df[["diagonal","height_left","height_right","margin_low","margin_up","length"]]
